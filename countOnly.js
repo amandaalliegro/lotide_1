e683f624ac858b0f0feb60031c7337f94691eba8
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-09-16 17:41:48 +0000

    count only implemented

--- countOnly.js
+++ countOnly.js
@@ -1,4 +1,4 @@
-/*This function should take in a collection of items and return counts for a specific subset of those items. 
+/*This function should take in a collection of items and return counts for a specific subset of those items.
 It won't count everything. In order to decide what to count, it will also be given an idea
  of which items we care about and it will only count those, ignoring the others.
 
@@ -15,7 +15,26 @@ const assertEqual = function(actual, expected) {
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
-}
+  const results = {};
+
+  for (const item of allItems) {
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+    console.log(item);
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+
+    return results;
+  }
+};
 
 
 const firstNames = [
@@ -29,6 +48,7 @@ const firstNames = [
   "Fang",
   "Joe"
 ];
+
 
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
