bb1a3b8e2e7503bfc35ddef6b6c0a014417d5237
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-09-16 20:53:14 +0000

    function implemented

--- eqObjects.js
+++ eqObjects.js
@@ -1,7 +1,32 @@
+const eqArrays = function(array1, array2) {
+  // if the number of elements inside the arrays are not the same, return false
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      // loops through the array and compares each index to the index of the other array. If they are not the same, returns false.
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
-  if(Object.keys(object1) === Object.keys(object2)) {
+  //first, verify if I have the same number of elements...
+  if(Object.keys(object1).length === Object.keys(object2).length) {
+    for (let key in object1) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])){
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (!(object1[key] === object2[key])) {
+        return false;
+      }
+    }
     return true;
   } else {
     return false;
