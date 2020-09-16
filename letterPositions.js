475e830e3a463cbce9ac818a83a739e071b79ff4
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-09-16 20:19:30 +0000

    commented

--- letterPositions.js
+++ letterPositions.js
@@ -1,10 +1,13 @@
 const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
+    //if my string is not empty...
     if (!(sentence[i] === " ")) {
+      //if I already have the element of my string, I just increment the index position by pushing it...
       if (results[sentence[i]]) {
         results[sentence[i]].push(i);
       } else {
+        // else, I include a new element AND the index
         results[sentence[i]] = [i];
       }
     }
