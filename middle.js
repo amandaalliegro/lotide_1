ed5d55657bd0649467d038b5e03768528e93bd19
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-09-16 13:19:35 +0000

    middle eslinted

--- middle.js
+++ middle.js
@@ -23,17 +23,17 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 // ACTUAL FUNCTION
-const middle = function (array) {
+const middle = function(array) {
   // array to be filled up
   const output = [];
   // if the length of the array is bigger than 3 elements AND it is an odd number...
   if (array.length >= 3 && array.length % 2 !== 0) {
     // the array 'output' will receive the middle element
-    console.log("odd" + output.push(array[(array.length - 1) / 2]))
+    console.log("odd" + output.push(array[(array.length - 1) / 2]));
     // if the length of the array is bigger than 3 elements...
   } else if (array.length >= 3) {
     // the array 'output' will receive the middle two elements
-    console.log("even" + output.push(array[array.length / 2 - 1], array[array.length / 2]))
+    console.log("even" + output.push(array[array.length / 2 - 1], array[array.length / 2]));
   }
   return output;
 };
