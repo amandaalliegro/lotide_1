795d6a70d6695653e27d1c225c8297b7afc781d2
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-09-16 17:32:56 +0000

    assertEqual added, test code added

---
+++ countOnly.js
@@ -0,0 +1,38 @@
+/*This function should take in a collection of items and return counts for a specific subset of those items. 
+It won't count everything. In order to decide what to count, it will also be given an idea
+ of which items we care about and it will only count those, ignoring the others.
+
+Items in our case will be limited to Strings.
+ */
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  else (console.log(`Assertion Failed: ${actual} !== ${expected}`));
+
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+}
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
