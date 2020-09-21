b1882535506b4861c61e70b9e3d5f21ba05af0df
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-09-21 19:53:42 +0000

    mocha chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,24 @@
-const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
+const assertArraysEqual = require('../assertArraysEqual');
+/*
 console.log(middle([1])); // => []
 console.log(middle([1, 2])); // => []
 console.log(middle([1, 2, 3])); // => [2]
 console.log(middle([1, 2, 3, 4, 5])); // => [3]
 console.log(middle([1, 2, 3, 4])); // => [2, 3]
 console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+*/
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert.strictEqual(middle([1]), []);
+  });
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.strictEqual(middle([1, 2, 3]), [2]); 
+  });
+  it("returns ['Lighthouse'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(middle(["Hello", "Lighthouse", "Labs"]), ['Lighthouse']); 
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,5 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+/*const assertEqual = require('../assertEqual');
+
 
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
@@ -9,3 +9,15 @@ assertEqual(words.length, 3); // original array should still have 3 elements!
 //TEST CODE
 console.log(assertEqual(tail([1, 2, 3, 4, 5, 6]).length,5));
 console.log(assertEqual(tail([1]).length,0));
+*/
+const assert = require('chai').assert;
+const tail = require('../tail');
+
+describe("#tail", () => {
+  it("returns 0 for [1]", () => {
+    assert.strictEqual(tail([1]).length, 0);
+  });
+  it("returns 5 for [1, 2, 3, 4, 5, 6]", () => {
+    assert.strictEqual(tail([1, 2, 3, 4, 5, 6]).length, 5);
+  });
+});
